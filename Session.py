28db806ac40a8598edb3c161063395d0b5b576b1
i-execute <Van.anime.maa@icloud.com>
2026-06-08 13:29:40 +0000

    update metadata

--- Session.py
+++ Session.py
@@ -1,5 +1,6 @@
 __version__ = (2, 2, 0)
 # meta developer: I_execute.t.me
+# meta banner: https://raw.githubusercontent.com/i-execute/Modules/main/Storage/Session/MetaBanner.jpeg
 
 import logging
 import asyncio
