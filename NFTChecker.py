5a506af313a1d5e980463d02b8d99e292e41d7fd
i-execute <Van.anime.maa@icloud.com>
2026-06-19 10:42:26 +0000

    Add meta banners

--- NFTChecker.py
+++ NFTChecker.py
@@ -1,5 +1,6 @@
 __version__ = (1, 4, 4)
 # meta developer: I_execute.t.me
+# meta banner: https://raw.githubusercontent.com/i-execute/Modules/main/Storage/NFTChecker/MetaBanner.jpeg
 
 import re
 import io
